fdcfd9621bd134264480406d483d120a507b78f8
Yury Kirpichev <ykirpichev@yandex-team.ru>
2018-01-05 22:14:39 +0300

    Add missed files

---
+++ Lesson06-Visualizer-Preferences/T06.02-Exercise-MakeAPreferenceFragment/app/src/main/java/SettingsFragment.java
@@ -0,0 +1,10 @@
+import android.example.com.visualizerpreferences.R;
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+public class SettingsFragment extends PreferenceFragmentCompat{
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.pref_visualizer);
+    }
+}
